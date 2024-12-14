4684c9b3df89c7ccba0629207c2c1fa6a4377021
Javi00C <javiercovas2000@gmail.com>
2024-12-14 16:53:38 +0100

    Added Tacto sensor to pybullet sim

--- gymnasium_env/envs/__init__.py
+++ gymnasium_env/envs/__init__.py
@@ -2,3 +2,4 @@ from gymnasium_env.envs.simple_robot import SimpleRobotEnv
 from gymnasium_env.envs.simple_robot_sine import SimpleRobotEnvSine
 from gymnasium_env.envs.simple_mujoco import SimpleMujocoEnv
 from gymnasium_env.envs.ur5e_2f85 import ur5e_2f85Env
+from gymnasium_env.envs.ur5e_2f85_pybullet import ur5e_2f85_pybulletEnv
